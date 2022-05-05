0f29da78512970e7cea0cbea3a8311e88e677eba
crioco <cerioco.03@gmail.com>
2022-05-06 03:11:43 +0800

    alarm added

--- lib/model/schedule_Alarm.dart
+++ lib/model/schedule_Alarm.dart
@@ -6,27 +6,27 @@ import 'alarm_info.dart';
 
 
 void scheduleAlarm(DateTime scheduledNotificationDateTime, AlarmInfo alarmInfo) async {
-    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'alarm_notif',
-      'alarm_notif',
-      channelDescription: 'your channel description',
-      icon: 'alarm_icon',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-      additionalFlags: Int32List.fromList(<int>[4]),
-      sound: const RawResourceAndroidNotificationSound('a_long_cold_sting'),
-      largeIcon: const DrawableResourceAndroidBitmap('alarm_icon'),
-    );
+  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    'alarm_notif',
+    'alarm_notif',
+    channelDescription: 'your channel description',
+    icon: 'alarm_icon',
+    importance: Importance.max,
+    priority: Priority.high,
+    ticker: 'ticker',
+    additionalFlags: Int32List.fromList(<int>[4]),
+    sound: const RawResourceAndroidNotificationSound('a_long_cold_sting'),
+    largeIcon: const DrawableResourceAndroidBitmap('alarm_icon'),
+  );
 
-    var iOSPlatformChannelSpecifics = const IOSNotificationDetails(
-        sound: 'a_long_cold_sting.wav',
-        presentAlert: true,
-        presentBadge: true,
-        presentSound: true);
-    var platformChannelSpecifics = NotificationDetails(
-        android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+  var iOSPlatformChannelSpecifics = const IOSNotificationDetails(
+      sound: 'a_long_cold_sting.wav',
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true);
+  var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
 
-    await flutterLocalNotificationsPlugin.schedule(0,alarmInfo.title, alarmInfo.alarmLabel, 
-        scheduledNotificationDateTime, platformChannelSpecifics);
-  }
+  await flutterLocalNotificationsPlugin.schedule(0,alarmInfo.title, alarmInfo.alarmLabel, 
+      scheduledNotificationDateTime, platformChannelSpecifics);
+}
